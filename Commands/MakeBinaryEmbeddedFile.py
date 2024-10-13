6685f107bc57b117b9eb10edbcd29b03f4c8d4c5
HaruoOgawa <haru.spring.yodai@gmail.com>
2024-10-13 12:47:24 +0900

    shader_minifierに失敗した時は元に戻すようにしました

--- Commands/MakeBinaryEmbeddedFile.py
+++ Commands/MakeBinaryEmbeddedFile.py
@@ -1,10 +1,26 @@
 import os
 import shutil
 import json
+import subprocess
 
-def DoShaderMin(src, dst):
+def DoShaderMin(src, dst, tmp):
+    # コマンド実行でエラーが出たときに戻すために退避させておく    
+    if(os.path.exists(dst)):
+        shutil.copyfile(dst, tmp)
+
     cmd = "Commands\compress_tools\shader_minifier.exe --format text --preserve-externals uniform -o %s %s" % (dst, src)
-    os.system(cmd)
+    result = subprocess.run(cmd, capture_output=True, text=True)
+
+    output = result.stdout
+
+    if(output != ""):
+        print("%s ________________________________\n" % (dst))
+        print("%s\n" % (output))
+        print("_________________________________\n")
+
+        # 元に戻す
+        if(os.path.exists(dst)):
+            shutil.copyfile(tmp, dst)
 
 def DoJSONMin(src, dst):
     with open(src, "rt") as f:
@@ -22,6 +38,7 @@ def Main():
     output = "Src/App/ScriptApp/"
     cppname = "CEmbeddedFileList"
     Prefix = "_Comp"
+    tmpPath = resouces + "Resources/Shaders/tmp.txt"
     
     FileList_Str = '"Resources/Font/dist/msdf.png" "Resources/MaterialFrame/BloomMix_MF.json" "Resources/MaterialFrame/Blur1Pass_MF.json" "Resources/MaterialFrame/Brigtness_MF.json" "Resources/MaterialFrame/ChromaticAberration_MF.json" "Resources/MaterialFrame/FrameTexture_MF.json" "Resources/MaterialFrame/LastCenter_MF.json" "Resources/MaterialFrame/LightShaft_MF.json" "Resources/MaterialFrame/MRTBlit_MF.json" "Resources/MaterialFrame/PBR_MF.json" "Resources/MaterialFrame/ReduceBuffer_MF.json" "Resources/MaterialFrame/RotRing_MF.json" "Resources/MaterialFrame/SSR_MF.json" "Resources/MaterialFrame/SSWaterMix_MF.json" "Resources/MaterialFrame/Water_MF.json" "Resources/MaterialFrame/blossum_MF.json" "Resources/MaterialFrame/centercube_01_mf.json" "Resources/MaterialFrame/cubegrid_mf.json" "Resources/MaterialFrame/mrt_renderer_mf.json" "Resources/MaterialFrame/sdftext_mf.json" "Resources/Scene/Demo-SESSIONS-2024.json" "Resources/Scene/Demo-SESSIONS-2024.tl" "Resources/Shaders/BloomMix.frag" "Resources/Shaders/Blur1Pass.frag" "Resources/Shaders/Brigtness.frag" "Resources/Shaders/ChromaticAberration.frag" "Resources/Shaders/LastCenter.frag" "Resources/Shaders/LightShaft.frag" "Resources/Shaders/MRTBlit.frag" "Resources/Shaders/ReduceBuffer.frag" "Resources/Shaders/RotRing.frag" "Resources/Shaders/SSR.frag" "Resources/Shaders/SSWaterMix.frag" "Resources/Shaders/Water.frag" "Resources/Shaders/blossum.frag" "Resources/Shaders/centercube_01.frag" "Resources/Shaders/cubegrid.frag" "Resources/Shaders/loadingbar.frag" "Resources/Shaders/loadingbar.vert" "Resources/Shaders/minimum.vert" "Resources/Shaders/mrt_renderer.frag" "Resources/Shaders/objectspace_raymarching.vert" "Resources/Shaders/pbr.frag" "Resources/Shaders/pbr.vert" "Resources/Shaders/renderboard.vert" "Resources/Shaders/sdftext.frag" "Resources/Shaders/texture.frag"'
 
@@ -42,7 +59,7 @@ def Main():
 
             if(extention == "vert" or extention == "frag"):
                 # Shader圧縮
-                DoShaderMin(srcPath, dstPath)
+                DoShaderMin(srcPath, dstPath, tmpPath)
             elif(extention == "json"):
                 # json圧縮
                 DoJSONMin(srcPath, dstPath)
